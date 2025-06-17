7b5ced09898103e2086f6384f7f7edbb3da20a37
dissolve <dissolve1882@naver.com>
2025-06-17 23:29:52 +0900

    Revert "v1.6"
    This reverts commit 7c8fe01293a5918a5e7ce19fc6ae367e39b58c02.

--- sql/crud.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import Session
-from . import models, schemas
-
-def get_latest_news(db: Session, limit: int = 30):
-    return db.query(models.News).order_by(models.News.id.desc()).limit(limit).all()
